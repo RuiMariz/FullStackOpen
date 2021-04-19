cd6fd5af8a2f6f156ea1bbf0e5a608c2210ba72a
Rui Mariz <ruivmariz@gmail.com>
2021-04-19 16:08:22 +0100

    Complete 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,45 +1,5 @@
 import React from 'react';
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Total = ({ course }) => {
-  const exercises = course.parts.map(part => part.exercises);
-  const total = exercises.reduce(
-    (accumulator, currentValue) => accumulator + currentValue, 0)
-  return (
-    <strong>Number of exercises {total}</strong>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  );
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from './Course';
 
 const App = () => {
   const courses = [
