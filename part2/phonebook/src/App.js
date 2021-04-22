23be508ebc184ce8f526adc45cbb075bae41ffb7
Rui Mariz <ruivmariz@gmail.com>
2021-04-22 18:06:38 +0100

    Complete 2.17-2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,7 +5,7 @@ const Persons = (props) => {
   return (
     <ul>
       {props.persons.map(person => <Person key={person.name} name={person.name}
-        number={person.number} nameFilter={props.nameFilter} />)}
+        number={person.number} nameFilter={props.nameFilter} removePerson={props.removePerson} />)}
     </ul>
   )
 }
@@ -13,7 +13,10 @@ const Persons = (props) => {
 const Person = (props) => {
   if (props.name.toLowerCase().includes(props.nameFilter.toLowerCase())) {
     return (
-      <li>{props.name} {props.number}</li>
+      <div>
+        <li>{props.name} {props.number}</li>
+        <button onClick={() => props.removePerson(props.name)}>remove</button>
+      </div>
     )
   }
   return null
@@ -29,7 +32,7 @@ const Filter = (props) => {
 
 const PersonForm = (props) => {
   return (
-    <form onSubmit={props.addName}>
+    <form onSubmit={props.addPerson}>
       <div>
         name: <input value={props.newName} onChange={props.handleNameChange} />
       </div>
@@ -54,11 +57,27 @@ const App = () => {
       })
   }, [])
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
-    const names = persons.map(person => person.name)
-    if (names.indexOf(newName) !== -1) {
-      alert(`${newName} is already added to phonebook`)
+    const person = persons.find(p => p.name === newName)
+    if (person) {
+      if (!window.confirm(`${person.name} is already added to the phonebook, replace the old number with the new one?`))
+        return
+      const noteObject = {
+        name: newName,
+        number: newNumber
+      }
+
+      personService
+        .update(person.id, noteObject)
+        .then(updatedPerson => {
+          setPersons(persons.map(p => p.id !== person.id ? p : updatedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(error => {
+          console.log(error)
+        })
       return
     }
 
@@ -89,16 +108,27 @@ const App = () => {
   const handleNameFilterChange = (event) => {
     setNewNameFilter(event.target.value)
   }
+  const removePerson = (name) => {
+    if (window.confirm(`Remove ${name}?`)) {
+      const person = persons.find(p => p.name === name)
+      personService
+        .remove(person.id)
+        .then(setPersons([...persons].filter(p => p.id !== person.id)))
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter nameFilter={nameFilter} handleNameFilterChange={handleNameFilterChange} />
       <h3>Add new number</h3>
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange}
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange}
         newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h3>Numbers</h3>
-      <Persons persons={persons} nameFilter={nameFilter} />
+      <Persons persons={persons} nameFilter={nameFilter} removePerson={removePerson} />
     </div>
   )
 }
