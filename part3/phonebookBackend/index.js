1b76db3ed72bf2e9f75f6afd0cab656fb78c4fdf
Rui Mariz <ruivmariz@gmail.com>
2021-04-26 18:46:46 +0100

    Complete 3.3-3.6

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -35,6 +35,52 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name)
+        return response.status(400).json({ error: 'name missing' })
+
+    if (!body.number)
+        return response.status(400).json({ error: 'number missing' })
+
+    if (persons.map(person => person.name).includes(body.name))
+        return response.status(400).json({ error: 'name already exists' })
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const generateId = () => {
+    return Math.trunc(Math.random() * 1000000)
+}
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
