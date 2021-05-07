b08f486bc19745443b2652f79a9f976725658dee
Rui Mariz <ruivmariz@gmail.com>
2021-05-07 22:03:04 +0100

    Complete 4.18-4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -18,18 +19,19 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findById(body.userId)
-    if (!user) {
-        response.status(400).json({ error: 'invalid user id' })
-        return
+    const token = request.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
     }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
         author: body.author || 'unknown',
         url: body.url,
         likes: body.likes || 0,
-        user: user.id
+        user: user._id
     })
 
     const savedBlog = await blog.save()
