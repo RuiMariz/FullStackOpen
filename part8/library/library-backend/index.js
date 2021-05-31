5f284cc22324191bc6498e0f694550602eb8ad53
Rui Mariz <ruivmariz@gmail.com>
2021-05-31 17:30:53 +0100

    Complete 8.14

--- part8/library/library-backend/index.js
+++ part8/library/library-backend/index.js
@@ -17,83 +17,85 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
     console.log('error connection to MongoDB:', error.message)
   })
 
+/*
 let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
+{
+  name: 'Robert Martin',
+  id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+  born: 1952,
+},
+{
+  name: 'Martin Fowler',
+  id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+  born: 1963
+},
+{
+  name: 'Fyodor Dostoevsky',
+  id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+  born: 1821
+},
+{
+  name: 'Joshua Kerievsky', // birthyear not known
+  id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+},
+{
+  name: 'Sandi Metz', // birthyear not known
+  id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+},
 ]
 
 let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
+{
+  title: 'Clean Code',
+  published: 2008,
+  author: 'Robert Martin',
+  id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+  genres: ['refactoring']
+},
+{
+  title: 'Agile software development',
+  published: 2002,
+  author: 'Robert Martin',
+  id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+  genres: ['agile', 'patterns', 'design']
+},
+{
+  title: 'Refactoring, edition 2',
+  published: 2018,
+  author: 'Martin Fowler',
+  id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+  genres: ['refactoring']
+},
+{
+  title: 'Refactoring to patterns',
+  published: 2008,
+  author: 'Joshua Kerievsky',
+  id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+  genres: ['refactoring', 'patterns']
+},
+{
+  title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+  published: 2012,
+  author: 'Sandi Metz',
+  id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+  genres: ['refactoring', 'design']
+},
+{
+  title: 'Crime and punishment',
+  published: 1866,
+  author: 'Fyodor Dostoevsky',
+  id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+  genres: ['classic', 'crime']
+},
+{
+  title: 'The Demon ',
+  published: 1872,
+  author: 'Fyodor Dostoevsky',
+  id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+  genres: ['classic', 'revolution']
+},
 ]
+*/
 
 const typeDefs = gql`
   type Author {
@@ -147,14 +149,15 @@ const resolvers = {
     findAuthor: (root, args) => Author.findOne({ name: args.name }),
 
     bookCount: () => Book.collection.countDocuments(),
-    allBooks: (root, args) => {
-      //if (!args.author && !args.genre)
-      return Book.find({})
-      /*
-      let returnedBooks = books
+    allBooks: async (root, args) => {
+      if (!args.author && !args.genre)
+        return Book.find({}).populate('author')
+
+      let returnedBooks = await Book.find({}).populate('author')
+
       if (args.author) {
         const byAuthor = (book) =>
-          args.author === book.author ? book.author : !book.author
+          args.author === book.author.name ? book.author : !book.author
         returnedBooks = returnedBooks.filter(byAuthor)
       }
       if (args.genre) {
@@ -163,15 +166,15 @@ const resolvers = {
         returnedBooks = returnedBooks.filter(byGenre)
       }
       return returnedBooks
-      */
+
     },
-    findBook: (root, args) => Book.findOne({ title: args.title })
+    findBook: (root, args) => Book.findOne({ title: args.title }).populate('author')
   },
 
   Author: {
-    bookCount: (root) => {
-      return 0
-      //return books.filter((book) => book.author === root.name).length
+    bookCount: async (root) => {
+      const books = await Book.find({}).populate('author')
+      return books.filter((book) => book.author.name === root.name).length
     }
   },
 
@@ -189,8 +192,13 @@ const resolvers = {
       return null
     },
 
-    addBook: (root, args) => {
-      const book = new Book({ ...args })
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        const newAuthor = new Author({ name: args.author })
+        author = await newAuthor.save()
+      }
+      const book = new Book({ title: args.title, published: args.published, author, genres: args.genres })
       return book.save()
     },
   }
